573e8bbd9f06d7c6762dc70760bf37d85054f20b
Kevin DeMarco <kevin.demarco@gmail.com>
2013-10-24 10:22:43 -0400

    initial commit

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf ./build
+rm -rf ./lib
+rm -rf ./bin
+rm -rf ./include
+
+# Future system clean
+#if [ "$1" == all ]; then
+#    rm -rf /usr/local/include/syllo
+#    rm -rf /usr/local/lib/libsylloproto*
+#fi
---
+++ src/hello-world/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+     cout << "Hello OpenCV Workbench!" << endl;
+     return 0;
+}
