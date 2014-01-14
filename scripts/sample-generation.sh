3cef155d16d7d12d9d9cc9a8fd874e38c4fc6f16
Kevin DeMarco <kevin.demarco@gmail.com>
2014-01-14 14:34:43 -0500

    fixed num pos issues

--- scripts/sample-generation.sh
+++ scripts/sample-generation.sh
@@ -13,8 +13,9 @@
 #
 # 
 
-WIDTH=20
-HEIGHT=20
+WIDTH=60
+HEIGHT=60
+NPOS=500
 
 # Create samples description file for positive images
 find ../cropped -name '*.JPG' -exec identify -format '%i 1 0 0 %w %h' \{\} \; > positives.dat
@@ -22,7 +23,10 @@ find ../cropped -name '*.JPG' -exec identify -format '%i 1 0 0 %w %h' \{\} \; > 
 # Create negative samples data file
 find ../../negatives -name '*.jpg' > negatives.dat
 
-perl ~/repos/opencv-workbench/scripts/createtrainsamples.pl positives.dat negatives.dat samples 1000 "opencv_createsamples  -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w $WIDTH -h $HEIGHT"
+# Second run...
+perl ~/repos/opencv-workbench/scripts/createtrainsamples.pl positives.dat negatives.dat samples $NPOS "opencv_createsamples  -bgcolor 0 -bgthresh 0 -maxxangle 0.01 -maxyangle 0.01 maxzangle 0.01 -maxidev 20 -w $WIDTH -h $HEIGHT"
+# First run...
+#perl ~/repos/opencv-workbench/scripts/createtrainsamples.pl positives.dat negatives.dat samples 1000 "opencv_createsamples  -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w $WIDTH -h $HEIGHT"
 #perl ~/repos/opencv-workbench/scripts/createtrainsamples.pl positives.dat negatives.dat samples 7000 "opencv_createsamples  -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w $WIDTH -h $HEIGHT"
 
 # Merge individual vector files
@@ -30,7 +34,7 @@ find samples/ -name '*.vec' > samples.dat
 ~/repos/opencv-workbench/bin/mergevec samples.dat samples.vec -w $WIDTH -h $HEIGHT
 
 # Create test samples
-perl ~/repos/opencv-workbench/scripts/createtestsamples.pl positives.dat negatives.dat tests 1000 "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 -maxzangle 0.5 maxidev 40"
+perl ~/repos/opencv-workbench/scripts/createtestsamples.pl positives.dat negatives.dat tests 1000 "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 -maxzangle 0.5 maxidev 40 -w $WIDTH -h $HEIGHT"
 
 # Create test samples data file
 find tests/ -name 'info.dat' -exec cat \{\} \; > tests.dat
--- scripts/training.sh
+++ scripts/training.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-WIDTH=20
-HEIGHT=20
+WIDTH=60
+HEIGHT=60
+NPOS=500
 
-opencv_haartraining -data haarcascade -vec samples.vec -bg negatives.dat -nstages 20 -nsplits 2 -minhitrate 0.999 -maxfalsealarm 0.5 -npos 1000 -nneg 3019 -w $WIDTH -h $HEIGHT -nonsym -mem 1500 -mode ALL
+opencv_haartraining -data haarcascade -vec samples.vec -bg negatives.dat -nstages 20 -nsplits 2 -minhitrate 0.999 -maxfalsealarm 0.5 -npos $NPOS -nneg 3019 -w $WIDTH -h $HEIGHT -nonsym -mem 1500 -mode ALL
 
 # Convert to XML
 ~/repos/opencv-workbench/bin/convert_cascade --size="$WIDTHx$HEIGHT" haarcascade haarcascade.xml
