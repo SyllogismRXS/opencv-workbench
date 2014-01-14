d8ea0267f8577f77ad12ef4552a7871b20757038
Kevin DeMarco <kevin.demarco@gmail.com>
2014-01-14 14:44:58 -0500

    traiing outputs to file now

--- scripts/training.sh
+++ scripts/training.sh
@@ -4,10 +4,10 @@ WIDTH=60
 HEIGHT=60
 NPOS=500
 
-opencv_haartraining -data haarcascade -vec samples.vec -bg negatives.dat -nstages 20 -nsplits 2 -minhitrate 0.999 -maxfalsealarm 0.5 -npos $NPOS -nneg 3019 -w $WIDTH -h $HEIGHT -nonsym -mem 1500 -mode ALL
+opencv_haartraining -data haarcascade -vec samples.vec -bg negatives.dat -nstages 20 -nsplits 2 -minhitrate 0.999 -maxfalsealarm 0.5 -npos $NPOS -nneg 3019 -w $WIDTH -h $HEIGHT -nonsym -mem 1500 -mode ALL | tee training-output.txt 2>&1
 
 # Convert to XML
 ~/repos/opencv-workbench/bin/convert_cascade --size="$WIDTHx$HEIGHT" haarcascade haarcascade.xml
 
 # Test the performance
-opencv_performance -data haarcascade.xml -info tests.dat -ni
+opencv_performance -data haarcascade.xml -info tests.dat -ni | tee performance-output.txt 2>&1
