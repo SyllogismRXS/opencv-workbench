687a59b8f5ddfa34ed97fc334a3fb82907fc5ae5
Kevin DeMarco <kevin.demarco@gmail.com>
2016-02-05 20:00:47 -0500

    fixing scripts

--- scripts/learn-test-threshold.sh
+++ scripts/learn-test-threshold.sh
@@ -12,7 +12,7 @@ YAML_PARAMS_FILE="empty"
 YAML_VIDEO_FILES="empty"
 K_FOLDS="3"
 SWEEP_PARAM="empty"
-NEG_TO_POS_RATIO="3"
+NEG_TO_POS_RATIO="2"
 
 # Use > 1 to consume two arguments per pass in the loop (e.g. each
 # argument has a corresponding value to go with it).
