4d432347df36cfb786f1fae65c64fcf57993f5fd
Kevin DeMarco <kevin.demarco@gmail.com>
2016-03-28 09:20:46 -0400

    minor edit

--- src/detector/Detector.h
+++ src/detector/Detector.h
@@ -4,7 +4,7 @@
 /// @file Detector.h
 /// @author Kevin DeMarco <kevin.demarco@gmail.com>
 ///
-/// Time-stamp: <2016-02-14 20:44:29 syllogismrxs>
+/// Time-stamp: <2016-03-23 15:09:50 syllogismrxs>
 ///
 /// @version 1.0
 /// Created: 04 Feb 2015
