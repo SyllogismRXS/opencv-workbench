cc9afa3bb2a82b46eb6ab63d0700ebf7bb83a3d6
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2016-06-11 09:56:30 -0400

    working on covar norm threshold

--- scripts/fin-detector-runs.sh
+++ scripts/fin-detector-runs.sh
@@ -3,10 +3,10 @@
 # Sweep for covariance threshold
 #./scripts/learn-test-velocity.sh -y ./data/yaml-range-params/covariance-threshold.yaml -f ./data/scenarios/velocity-train-test.yaml -s covar_threshold -h 1
 
-./scripts/learn-test-velocity.sh -y ./data/yaml-range-params/covariance-threshold.yaml -f ./data/scenarios/velocity-train-test.yaml -s min_velocity_threshold_2 -h 1
+#./scripts/learn-test-velocity.sh -y ./data/yaml-range-params/covariance-threshold.yaml -f ./data/scenarios/velocity-train-test.yaml -s min_velocity_threshold_2 -h 1
 
 #./scripts/learn-test-velocity.sh -y ./data/yaml-range-params/covariance-threshold.yaml -f ./data/scenarios/velocity-train-test.yaml -s class_age_confirmed -h 1
 
 #./scripts/learn-test-velocity.sh -y ./data/yaml-range-params/covariance-threshold.yaml -f ./data/scenarios/velocity-train-test.yaml -s class_age_confirmed -h 1
 
-#./scripts/learn-test-velocity.sh -y ./data/yaml-range-params/covariance-threshold.yaml -f ./data/scenarios/velocity-train-test.yaml -s covar_norm_threshold -h 1
+./scripts/learn-test-velocity.sh -y ./data/yaml-range-params/covariance-threshold.yaml -f ./data/scenarios/velocity-train-test.yaml -s covar_norm_threshold -h 1
